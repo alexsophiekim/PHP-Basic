277914bf6adf1cc98a4847a94c6bb07ba1c3e945
Sophie Kim <270010565@bun2-206-23.local>
2019-10-08 13:36:31 +1300

    basic 1st

---
+++ arrays.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Arrays</title>
+  </head>
+  <body>
+    <?php
+        $numbers = array(4,8,15,16,23,42);
+        echo $numbers[1];
+      ?>
+
+      <?php $mixed = array(6,"fox","dog",array("x","y","z")); ?><br/>
+      <?php echo $mixed[2] ?><br/>
+      <?php echo $mixed[3] ?><br/>
+      <?php echo $mixed; ?><br/>
+      <pre>
+      <?php echo print_r($mixed); ?>
+      </pre>
+      <?php echo $mixed[3][1]; ?><br/>
+      <?php $mixed[2] ="cat"; ?>
+      <?php $mixed[4] = "mouse"; ?>
+      <?php $mixed[] = "horse"; ?>
+      <pre>
+      <?php echo print_r($mixed); ?>
+      </pre>
+
+      <?php
+      // Shorter way - new version
+        $array = [1,2,3];
+       ?>
+  </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Variables</title>
+  </head>
+  <body>
+    <?php
+        $var1 = 10;
+        echo $var1;
+        echo '<br/>';
+
+        $var1 = 100;
+        echo $var1;
+        echo '<br/>';
+     ?>
+  </body>
+</html>
